215bd17d7f36f69a70b40717f6e0a02ccd1f1540
Chuck LU <chuck.lu@edenred.com>
2021-08-26 18:04:10 +0800

    test SanitizeForInjection with true and false

--- CsvHelperUnitTest/CsvHelperUnitTest.cs
+++ CsvHelperUnitTest/CsvHelperUnitTest.cs
@@ -39,8 +39,27 @@ namespace CsvHelperUnitTest
                 new Foo { Id = 2, Name = "=AND(1,2)" },
             };
             
-            using (var writer = new StreamWriter($@"C:\workspace\Company\UK\Troubleshooting\test-{DateTime.Now:yyyyMMdd-HHmmss}.csv"))
+            using (var writer = new StreamWriter($@"C:\workspace\Company\UK\Troubleshooting\TestSanitizeForInjection_True-{DateTime.Now:yyyyMMdd-HHmmss}.csv"))
             using (var csv = new CsvWriter(writer,config ))
+            {
+                csv.WriteRecords(records);
+            }
+        }
+
+        [Test]
+        public void Test20210826_003()
+        {
+            var config = new CsvConfiguration(new CultureInfo("en-GB"));
+            //config.SanitizeForInjection = false;//default value is false
+
+            var records = new List<Foo>
+            {
+                new Foo { Id = 1, Name = "one" },
+                new Foo { Id = 2, Name = "=AND(1,2)" },
+            };
+
+            using (var writer = new StreamWriter($@"C:\workspace\Company\UK\Troubleshooting\TestSanitizeForInjection_False-{DateTime.Now:yyyyMMdd-HHmmss}.csv"))
+            using (var csv = new CsvWriter(writer, config))
             {
                 csv.WriteRecords(records);
             }
