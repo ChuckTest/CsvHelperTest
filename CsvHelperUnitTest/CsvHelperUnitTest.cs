1ef7348e4a04cefb7800c9141d0849da9c8ab4db
Chuck LU <chuck.lu@edenred.com>
2021-08-27 17:49:46 +0800

    test quotes

--- CsvHelperUnitTest/CsvHelperUnitTest.cs
+++ CsvHelperUnitTest/CsvHelperUnitTest.cs
@@ -66,5 +66,27 @@ namespace CsvHelperUnitTest
                 csv.WriteRecords(records);
             }
         }
+
+        [Test]
+        public void Test20210827_001()
+        {
+            var config = new CsvConfiguration(new CultureInfo("en-GB"));
+            config.SanitizeForInjection = true;
+
+            var records = new List<Foo>
+            {
+                new Foo { Id = 1, Name = "one" },
+                new Foo { Id = 2, Name = "=AND(1,2)" },
+                new Foo { Id = 3, Name = "\"123\"" },
+                new Foo { Id = 4, Name = "\"=AND(1,2)\"" },
+                new Foo { Id = 5, Name = "\"=AND(1,2)" },
+            };
+
+            using (var writer = new StreamWriter($@"C:\workspace\Company\UK\Troubleshooting\TestSanitizeForInjection_False-{DateTime.Now:yyyyMMdd-HHmmss}.csv"))
+            using (var csv = new CsvWriter(writer, config))
+            {
+                csv.WriteRecords(records);
+            }
+        }
     }
 }
